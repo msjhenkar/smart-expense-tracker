0a94679dd9ced89df1fee5dcec51d334741a0a58
Jhenkar MS <jhenkar5nov3@gmail.com>
2026-07-31 22:04:29 +0530

    Implemented Controller, service, Repository layer

--- src/main/java/com/expense_tracker/Controller/ExpenseController.java
+++ src/main/java/com/expense_tracker/Controller/ExpenseController.java
@@ -1,11 +1,69 @@
 package com.expense_tracker.Controller;
 
+import com.expense_tracker.Service.ExpenseService;
+import com.expense_tracker.dto.ExpenseRequest;
+import com.expense_tracker.model.Expense;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-
-
-
-
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExpenseController {
+    private final ExpenseService service;
 
+    public ExpenseController (ExpenseService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> addExpense(@RequestBody ExpenseRequest request){
+        Expense expense = new Expense(
+                null,
+                request.getTitle(),
+                request.getAmount(),
+                request.getCategory(),
+                request.getDate()
+        );
+        Expense saved = service.addExpense(expense);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Expense>> getExpenses(@RequestParam(required = false) String category){
+        List<Expense> expenses = (category == null)
+                ? service.getAllExpense()
+                : service.getExpenseByCategory(category);
+
+        return ResponseEntity.ok(expenses);
+
+    }
+
+    @GetMapping("/total")
+    public ResponseEntity<Map<String, BigDecimal>> getTotal(@RequestParam(required = false) String category){
+        BigDecimal total;
+
+        if(category == null){
+            total = service.getTotal();
+        }else {
+            total = service.getTotalByCategory(category);
+        }
+
+        Map<String, BigDecimal> response = new HashMap<>();
+        response.put("total", total);
+
+        return ResponseEntity.ok(response);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExpense(@PathVariable Long id) {
+        service.deleteExpense(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/expense_tracker/Service/ExpenseService.java
+++ src/main/java/com/expense_tracker/Service/ExpenseService.java
@@ -1,11 +1,25 @@
 package com.expense_tracker.Service;
 
+import com.expense_tracker.model.Expense;
 import com.expense_tracker.repository.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Service
 public interface ExpenseService {
+    Expense addExpense(Expense expense);
 
+    List<Expense> getAllExpense();
+
+    List<Expense> getExpenseByCategory(String category);
+
+    BigDecimal getTotal();
+
+    BigDecimal getTotalByCategory(String category);
+
+    void deleteExpense(Long id);
 
 }
